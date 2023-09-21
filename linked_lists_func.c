220d1c864d780125470c84584cad099626d32bb1
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-21 14:43:40 -0700

    Adding linked lists functions

--- linked_lists_func.c
+++ linked_lists_func.c
@@ -1,6 +1,40 @@
 #include "monty.h"
 
 /**
+  * add_dnodeint - adds a new node at the beginning of a list
+  * @head: double pointer
+  * @n: the element of a new node
+  * Return: the address of the new node
+  */
+stack_t *add_dnodeint(stack_t **head, const int n)
+{
+	stack_t *new = NULL;
+
+	new = (stack_t *)malloc(sizeof(stack_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+
+	if (!(*head))
+	{
+		*head = new;
+		return (new);
+	}
+
+	if ((*head)->prev)
+	{
+		*head = (*head)->prev;
+	}
+
+	(*head)->prev = new;
+	*head = new;
+
+	return (new);
+}
+
+/**
  * add_dnodeint_end - adds a new node at the end
  * of a dlistint_t list
  *
@@ -11,7 +45,7 @@
 stack_t *add_dnodeint_end(stack_t **head, const int n)
 {
 	stack_t *new, *temp = *head;
-	
+
 	new = (stack_t *)malloc(sizeof(stack_t));
 	if (!new)
 		return (NULL);
