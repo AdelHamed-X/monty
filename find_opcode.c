bd437420cc3cd35f2d84421a9a56426330cabd9e
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-21 14:19:21 -0700

    push the correct

---
+++ find_opcode.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ *
+ */
+void (*find_opcode(char *input_code))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t opcode[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; opcode[i].f != NULL; i++)
+	{
+		if (strcmp(opcode[i].opcode, input_code) == 0)
+			break;
+	}
+	return (opcode[i].f);
+}
